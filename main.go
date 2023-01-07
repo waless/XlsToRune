e6b07b168d10cb12d35ce1a7f069a7093a3fd25e
waless <waless.seel@live.jp>
2023-01-08 01:37:24 +0900

    入力引数から出力デフォルトパス設定を設定するように

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
+// ---定数----
+//
+// ファイル拡張子
+const RuneExt = ".rune"
 
 func main() {
-	fmt.Println("HelloWorld")
+	// 入力ファイルパス(必須)
+	// 先頭に引数名なしで書かれている事を意図している
+	var in = ""
+	if len(os.Args) > 1 {
+		in = os.Args[1]
+	}
+
+	// 補助引数(なくても良い)
+	var out_default = makeOutputDefaultPath(in)
+	var pout = flag.String("o", out_default, "出力ファイルパス")
+
+	flag.Parse()
+
+	fmt.Println(*pout)
+}
+
+func makeOutputDefaultPath(input_path string) string {
+	var in_ext = path.Ext(input_path)
+	var none_ext = strings.Replace(input_path, in_ext, "", 1)
+	return none_ext + RuneExt
 }
