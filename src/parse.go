ac568404f73f6fc2dd0ce47985f09f02a8a79f30
waless <waless.seel@live.jp>
2023-01-09 03:30:55 +0900

    タイプ定義のパース部分の枠組み作成 実装中

--- src/parse.go
+++ src/parse.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -9,11 +10,21 @@ import (
 type ERuneType int
 
 const (
-	Type ERuneType = iota
-	Enum
-	Int
-	Float
-	String
+	EType ERuneType = iota
+	EEnum
+	EInt
+	EFloat
+	EString
+)
+
+const (
+	SRuneType = "RuneType"
+	SType     = "type"
+	SEnum     = "enum"
+	SString   = "string"
+	SInt      = "int"
+	SFloat    = "float"
+	SComment  = "#"
 )
 
 type RuneTypeName struct {
@@ -24,6 +35,7 @@ type RuneTypeName struct {
 type RuneTypeValue struct {
 	typeName   RuneTypeName
 	valueArray []string
+	colIndex   int
 }
 
 type RuneTypeTable struct {
@@ -41,13 +53,28 @@ type RuneTypeBook struct {
 	sheets []RuneTypeSheet
 }
 
-func ParseXls(path string) RuneTypeBook {
-	var result RuneTypeBook
+type contextType int
 
+const (
+	ContextNone contextType = iota
+	ContextTable
+)
+
+type context struct {
+	runeBook     RuneTypeBook
+	currentType  contextType
+	currentTable RuneTypeTable
+	rowIndex     int
+	colIndex     int
+}
+
+var gctx context
+
+func ParseXls(path string) RuneTypeBook {
 	file, err := excelize.OpenFile(path)
 	if err != nil {
 		fmt.Println(err)
-		return result
+		return gctx.runeBook
 	}
 
 	sheets := file.GetSheetList()
@@ -57,19 +84,139 @@ func ParseXls(path string) RuneTypeBook {
 		rows, err := file.Rows(name)
 		if err != nil {
 			fmt.Println(err)
-			return result
+			return gctx.runeBook
 		}
 
+		gctx.rowIndex = 0
 		for rows.Next() {
 			cols, err := rows.Columns()
 			if err != nil {
 				fmt.Println(err)
-				return result
+				return gctx.runeBook
 			}
 
-			for _, col := range cols {
-				fmt.Printf("row: %v\n", col)
+			parseCols(cols)
+			gctx.rowIndex++
+		}
+	}
+
+	return gctx.runeBook
+}
+
+func parseCols(cols []string) {
+	gctx.colIndex = 0
+
+	switch gctx.currentType {
+	case ContextNone:
+		parseColsForNone(cols)
+	case ContextTable:
+		parseColsForTable(cols)
+	}
+}
+
+func parseColsForNone(cols []string) {
+	for _, col := range cols {
+		switch col {
+		case SRuneType:
+			newCurrentTable(cols)
+			return
+		}
+		gctx.colIndex++
+	}
+}
+
+func parseColsForTable(cols []string) {
+}
+
+func newCurrentTable(cols []string) error {
+	gctx.currentTable = RuneTypeTable{}
+	for _, col := range cols {
+		value := strings.Trim(col, " ")
+
+		if isComment(value) {
+		} else if strings.Contains(value, SEnum) {
+			v := parseSEnum(col)
+			gctx.currentTable.values = append(gctx.currentTable.values, v)
+		} else {
+			err := checkTypeValidity(value)
+			if err != nil {
+				return err
+			}
+
+			if strings.Contains(value, SType) {
+				parseSType(col)
+			} else if strings.Contains(value, SString) {
+				parseSString(col)
+			} else if strings.Contains(value, SInt) {
+				parseSInt(col)
+			} else if strings.Contains(value, SFloat) {
+				parseSFloat(col)
 			}
 		}
 	}
+
+	return nil
+}
+
+func checkTypeValidity(str string) error {
+	strs := strings.Split(str, ":")
+	if len(strs) == 2 {
+		return nil
+	} else {
+		return fmt.Errorf("%s : invalid type string : %s", makeRuneErrorPrefix(), str)
+	}
+}
+
+func makeRuneErrorPrefix() string {
+	return fmt.Sprintf("row, %d, col %d", gctx.rowIndex, gctx.colIndex)
+}
+
+func parseSType(str string) error {
+	str_array := parseTypeString(str)
+	if len(str_array) > 1 {
+		name := str_array[0]
+		if str_array[1] != SType {
+			return fmt.Errorf("row %d, col %d, not found RuneType", gctx.rowIndex, gctx.colIndex)
+		}
+		gctx.currentTable.name = name
+	}
+
+	return nil
+}
+
+func parseSEnum(str string) RuneTypeValue {
+	result := RuneTypeValue{}
+	result.typeName.kind = EType
+
+	return result
+}
+
+func parseSString(str string) (RuneTypeValue, error) {
+	result := RuneTypeValue{}
+
+	result.typeName.kind = EString
+
+	str_array := parseTypeString(str)
+	if len(str_array) > 1 {
+	}
+
+	return result, nil
+}
+
+func parseSInt(str string) {
+}
+
+func parseSFloat(str string) {
+}
+
+func isComment(str string) bool {
+	return strings.Contains(str, SComment)
+}
+
+func strToSType(str string) {
+}
+
+func parseTypeString(str string) []string {
+	str = strings.Trim(str, " ")
+	return strings.Split(str, ":")
 }
